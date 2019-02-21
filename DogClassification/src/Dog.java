31d303257246052d133cb8ae9c90c98f92b853cc
brandonhardesty <brandonhardesty@172.24.1.243>
2019-02-21 13:28:08 -0500

    blah

---
+++ DogClassification/src/Dog.java
@@ -0,0 +1,13 @@
+
+public class Dog {
+	private int[] attributes;
+	
+	public Dog(int[] attributes) {
+		this.attributes = attributes;
+		
+	}
+	public int[] getAttributes() {
+		return attributes;
+	}
+
+}
