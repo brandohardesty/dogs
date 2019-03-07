935190498e4cb96e083777828207826aee081dd2
acb4905 <37702798+acb4905@users.noreply.github.com>
2019-03-07 12:57:06 -0500

    Brute force started

--- DogClassification/src/BruteForce.java
+++ DogClassification/src/BruteForce.java
@@ -1,4 +1,28 @@
+import java.lang.ArrayList;
+import java.lang.Math;
 
 public class BruteForce {
-
+	
+	private int[] userInputs;
+	private Dog[] dogList;
+	
+	BruteForce(int[] userInputs, Dog[] dogList){
+		this.userInputs=userInputs;
+		this.dogList=dogList;
+	}
+	
+	private int[] compareCharacteristics() {
+		ArrayList<int> comparisons=new ArrayList<int>();
+		//For every dog breed
+		for (Dog d : dogList) {
+			//Go through all 12 characteristics
+			for (int i; i<12; i++) {
+				//Subtract 
+				comparisons.add(Math.abs(userInputs.get(i)-(d.getAttributes()[i]));
+			}
+		}
+		return comparisons;
+	}
+	
+	
 }
