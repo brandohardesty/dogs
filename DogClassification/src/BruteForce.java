39396e0e4b4ff713ba00cd09b789a4373a3df818
Audrey Bunn <37702798+acb4905@users.noreply.github.com>
2019-03-19 18:16:34 -0400

    Brute force Updated

--- DogClassification/src/BruteForce.java
+++ DogClassification/src/BruteForce.java
@@ -1,27 +1,44 @@
-import java.util.ArrayList;
+import java.lang.ArrayList;
+import java.lang.Math;
 
 public class BruteForce {
 	
 	private int[] userInputs;
 	private Dog[] dogList;
+	ArrayList<Integer> comparisons;
 	
 	BruteForce(int[] userInputs, Dog[] dogList){
 		this.userInputs=userInputs;
 		this.dogList=dogList;
+		this.comparisons=new ArrayList<Integer>();
 	}
 	
-	private ArrayList<Integer> compareCharacteristics() {
-		ArrayList<Integer> comparisons=new ArrayList<Integer>();
+	private static compareCharacteristics() {
 		//For every dog breed
 		for (Dog d : dogList) {
 			//Go through all 12 characteristics
-			for (int i = 0; i<12; i++) {
-				//Subtract 
+			for (int i=0; i<12; i++) {
+				//Subtract user dog characteristic from user inputs and take the absolute value
 				comparisons.add(Math.abs(userInputs[i]-(d.getAttributes()[i])));
 			}
 		}
-		return comparisons;
 	}
 	
+	private void bubbleSort(int arr[]){
+		int n = arr.length;
+		for (int i = 0; i < n-1; i++)
+			for (int j = 0; j < n-i-1; j++)
+				if (arr[j] > arr[j+1]){
+					// swap temp and arr[i]
+					int temp = arr[j];
+					arr[j] = arr[j+1];
+					arr[j+1] = temp;
+	            }
+	} 
+	
+	private int findBestMatch() {
+		compareCharacteristics();
+		bubbleSort((Array)characteristics);
+	}
 	
 }
